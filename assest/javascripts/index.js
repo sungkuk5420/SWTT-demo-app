955e475ace74cfe174f0307567a742606c38fe06
sungkuk5420 <sungkuk5420@gmail.com>
2019-09-10 23:55:50 +0900

    投稿3段階完成

--- assest/javascripts/index.js
+++ assest/javascripts/index.js
@@ -1,16 +1,25 @@
 const homeHeader = document.querySelector('.header.home');
+
 const feedHeader1 = document.querySelector('.header.feed1');
 const feedHeader2 = document.querySelector('.header.feed2');
+const feedHeader3 = document.querySelector('.header.feed3');
+
 const mainPage = document.querySelector('.page-container.main-page');
+
 const feedPage1 = document.querySelector('.page-container.feed-page1');
 const feedPage2 = document.querySelector('.page-container.feed-page2');
+const feedPage3 = document.querySelector('.page-container.feed-page3');
+
 const mainImage1 = document.querySelector('.page-container.main-page .main-image1');
 const mainImage2 = document.querySelector('.main-image2');
+
 const imageSliderNav = document.querySelector('.image-slider-nav');
 const contentName = document.querySelector('.store-info__content__name2');
 const date = document.querySelector('.date-text .date');
 const contentText = document.querySelector('.content-text');
 const likeText = document.querySelector('.like-text');
+const feed3Textarea = feedPage3.querySelector('.form-wrapper textarea');
+
 const footer = document.querySelector('.footer');
 window.home = () => {
     location.reload();
@@ -51,4 +60,33 @@ window.moveFeed2 = () => {
     feedPage2.classList.remove('hide');
     feedHeader1.classList.add('hide');
     feedHeader2.classList.remove('hide');
+}
+
+window.moveFeed3 = () => {
+    feedPage2.classList.add('hide');
+    feedPage3.classList.remove('hide');
+    feedHeader2.classList.add('hide');
+    feedHeader3.classList.remove('hide');
+}
+
+window.animationKey = false;
+window.keyboardAnimation = () => {
+    if(animationKey){
+        return false;
+    }
+    animationKey = true;
+    var text = '伊勢丹メンズ館でオーダーしたシャツが最高過ぎ！迷っていたシャツも買っちゃおうかな〜 #isetanmens #カスタムオーダーシャツ';
+    feed3Textarea.value = '';
+    for(var i=0,len=text.length; i<len; i++){
+        delayAnimation(text,i);
+    }
+
+    function delayAnimation(text,index) {
+        setTimeout(function(){
+            feed3Textarea.value = feed3Textarea.value+text[index]; 
+            if(text.length-1 == index){
+                animationKey = false;
+            }
+        },100*index)
+    }
 }
